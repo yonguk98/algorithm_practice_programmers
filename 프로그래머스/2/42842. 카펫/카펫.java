e857a66e147031d9f7c208a1203bdff73c41af50
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2024-11-14 15:26:27 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,23 +4,14 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
 
-        List<Integer[]> list = new ArrayList<>();
         for (int i = 1; i <= yellow; i++){
             if(yellow % i == 0){
-                list.add(new Integer[]{yellow/i,i});
+                if((yellow/i + i) * 2 + 4 == brown){
+                    answer = new int[]{yellow/i + 2,i + 2};
+                    break;
+                }
             }
 
-            if(yellow/i <= i){
-                break;
-            }
-
-        }
-
-        for(Integer[] i : list){
-            if(i[0]*2 + i[1]*2 + 4 == brown){
-                answer = new int[]{i[0]+2, i[1]+2};
-                break;
-            }
         }
         return answer;
     }
