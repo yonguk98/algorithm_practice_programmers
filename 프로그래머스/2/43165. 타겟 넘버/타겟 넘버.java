4aa65bc1a78c3b00e58453b18d9f314f4c733dc5
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2024-11-15 23:10:15 +0900

    [level 2] Title: 타겟 넘버, Time: 5.85 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,26 @@
+class Solution {
+    static boolean[] visited;
+    static int[] Numbers;
+    static int Target;
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        visited = new boolean[numbers.length];
+        Numbers = numbers;
+        Target = target;
+
+        dfs(0,numbers.length,0);
+
+        return answer;
+    }
+    
+    public static void dfs(int r, int n, int sum){
+        if(r==n){
+            if(sum==Target) answer+=1;
+            return;
+        }
+
+        dfs(r+1,n,sum+Numbers[r]);
+        dfs(r+1,n,sum-Numbers[r]);
+    }
+}
